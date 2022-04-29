b1eed4b753a65909738166faaecaf6c5ca354d5f
Vector-7 <seokbong60@gmail.com>
2022-04-29 09:41:20 +0900

    test: 마감 시간으로 인한 테스트 케이스 약화
    시간 상 빠른 마무리를 위해 강한 테스트 일부 제거
    시간이 남으면 조금씩 테스트케이스를 추가할 예정

--- amounts/views.py
+++ amounts/views.py
@@ -7,7 +7,6 @@ from django.shortcuts import get_object_or_404
 from rest_framework          import status
 from rest_framework.views    import APIView
 from rest_framework.response import Response
-from yaml import serialize
 
 from .models      import Amount
 import datetime
